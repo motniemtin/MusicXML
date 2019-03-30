e773542882b078b478bef2a8e842c9086e2ccebd
Lam Nguyen <motniemtin@gmail.com>
2019-03-30 09:40:24 +0100

    add code

--- src/Motniemtin/MusicXML/MusicXML.php
+++ src/Motniemtin/MusicXML/MusicXML.php
@@ -1,5 +1,15 @@
 <?php
 namespace Motniemtin\MusicXML;
 class MusicXML{
-  
+  var $xml;
+  function __construct($xmlPath){
+    try{
+      $this->xml=simplexml_load_string($note);
+    }catch(Exception $e){
+      throw new Exception("Error when load xml file..");
+    }    
+  }
+  function makeJson(){
+    
+  }
 }
