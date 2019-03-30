d34f4a7a20c68cf27393e8a74e27e370e58097d7
Lam Nguyen <motniemtin@gmail.com>
2019-03-30 09:51:16 +0100

    remove test

--- src/Motniemtin/MusicXML/MusicXML.php
+++ src/Motniemtin/MusicXML/MusicXML.php
@@ -5,11 +5,12 @@ class MusicXML{
   function __construct($xmlPath){
     try{
       $this->xml=simplexml_load_string($note);
+      $this->makeJson();
     }catch(Exception $e){
       throw new Exception("Error when load xml file..");
     }    
   }
   function makeJson(){
-    echo "here";
+    print_r($this->xml);
   }
 }
